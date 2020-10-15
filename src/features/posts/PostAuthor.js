173ed3238b13388b498392f480f505d577850cfe
briantran98 <98brian.tran@gmail.com>
2020-10-15 16:37:34 -0700

    Added timestamp and sorted posts to be in descending time order

--- src/features/posts/PostAuthor.js
+++ src/features/posts/PostAuthor.js
@@ -3,7 +3,6 @@ import { useSelector } from 'react-redux'
 
 export const PostAuthor = ({ userId }) => {
     const author = useSelector(state =>{
-        console.log(userId)
             return state.users.find(user => user.id === userId)
     }
         )
